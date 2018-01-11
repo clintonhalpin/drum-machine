162e50e71091e1b3c3aec1e54dd39985a9c023af
clintonhalpin <clintonhalpin@gmail.com>
2018-01-10 22:39:26 -0500

    WIP

--- app.js
+++ app.js
@@ -1,224 +1,51 @@
 /**
- * All THE FUNCTIONAL STUFF
+ * UI Components
  */
+const app = document.getElementById('app')
+const playPause = document.getElementById('playPause')
+const speed = document.getElementById('speed')
 
-let interval
+/**
+ * State
+ */
 let state = {
-	playing: false,
 	currIteration: 0,
-	timeout: 128,
+	data: false,
+	interval: false,
 	iterations: 16,
-	lastNodes: []
+	lastNodes: [],
+	playing: false,
+	timeout: 128,
 }
 
-let playPause = document.getElementById('playPause')
-let speed = document.getElementById('speed')
-speed.addEventListener('change', changeRate)
+/**
+ * Hook up our UI Event listeners
+ */
+speed.addEventListener('change', modifyBPM)
 playPause.addEventListener('click', togglePlay) 
 
-function togglePlay() {
-	state.playing ? stop() : play()
-}
+/**
+ * [runSequence description]
+ * @return {[type]} [description]
+ */
+function runSequence(){
 
-function execute() {		
-	interval = setInterval(() => {
-
-		if( state.lastNodes.length > 0 ) {
-			state.lastNodes.map(node => {
-				toggleClass(node, 'beat-on')
-			})
-			state.lastNodes = []
-		}
-
-		state = Object.assign(state, {
-			currIteration: state.currIteration < (state.iterations - 1) ? state.currIteration + 1 : 0
-		})
-		
-		let nodes = document.querySelectorAll('.beat-' + state.currIteration);
-		for(var i = 0; i < nodes.length; i++) {
-			state.lastNodes.push(nodes[i])
-			toggleClass(nodes[i], 'beat-on')
-		}
-
-	}, state.timeout)
-}
-
-function play() {
-	console.log('SEQUENCER: PLAY')
-	state = Object.assign(state, {
-		playing: true
-	})
-	execute()
-}
-
-function stop() {
-	console.log('SEQUENCER: STOP')
-	state = Object.assign(state, {
-		playing: false
-	})
-	clearInterval(interval)
-}
-
-function changeRate( e ) {
-	console.log('SEQUENCER: CHANGE RATE')
-	let rate = e.target.value
-	state = Object.assign(state, {
-		timeout: Math.floor(60000 / rate)
-	})
-	stop()
-	play()
 }
 
 /**
- * ALL THE RENDER STUFFF
+ * [modifyBPM description]
+ * @return {[type]} [description]
  */
+function modifyBPM(){
 
-/**
- * Listen for DOMContentLoaded -> Kick the tires and Light the fires
- * https://www.youtube.com/watch?v=2F1bMG9wzXY
- */
-let app;
-document.addEventListener("DOMContentLoaded", boot)
-
-/**
- * [boot Start the app]
- * @return {null, side-effect}
- */
-function boot() {
-	app = document.getElementById('app')
-	const ui = fullRender(getSequence(), getControls())
-	appendToEl(app, ui)
 }
 
 /**
- * [fullRender description]
- * @param  {[type]} data [description]
- * @return {[type]}      [description]
+ * [togglePlay description]
+ * @return {[type]} [description]
  */
-function fullRender(data, controls) {
-	
-	let sequenceLength = 0;
-	data.pattern.seq.patternTracks.map(track => {
-		if( track.steps.length > sequenceLength ) {
-			sequenceLength = track.steps.length
-		}
-	})
+function togglePlay(){
 
-	let numbers = "";
-	for(var i = 0; i < sequenceLength; i++) {
-		numbers += renderNumberColumn({ idx: i + 1 })
-	}
-
-	numbers = ['<div class="flex space-evenly">', numbers, '</div>'].join('')
-
-	let tracks = data.pattern.seq.patternTracks.map((track, idx) => {
-		
-		let trackStr = track.steps.map((beat, jdx) => {
-			return renderBeat({ active : beat, trackIdx: idx, beatIdx: jdx  })
-		}).join('')
-
-		return ['<div class="flex space-evenly">', trackStr, '</div>'].join('')
-
-	}).join('');
-
-	return numbers + tracks
-}
-
-function renderBeat( data ) {
-	if( data.active ) {
-		return template(`
-			<div class="col-15">
-				<div class="beat beat--active track-{trackIdx} beat-{beatIdx}">
-				    <div class="beat-inner"></div>
-				</div>
-			</div>
-		`, data)
-	} else {
-		return template(`
-			<div class="col-15">
-				<div class="beat">
-				    <div class="beat-inner track-{trackIdx} beat-{beatIdx}"></div>
-				</div>
-			</div>
-		`, data)
-	}
-}
-
-function renderNumberColumn( data ) {
-	return template(`
-		<div class="col-15">
-		  <div class="beat-number">{idx}</div>
-		</div>
-	`, data)
-}
-
-function renderColumn( data ) {
-	return template(`
-		<div class="col-15">
-		  <div class="beat-number">{idx}</div>
-		  {beats}
-		</div>
-	`, data)
-}
-
-/**
- * [render description]
- * @param  {[type]} el   [description]
- * @param  {[type]} data [description]
- * @return {[type]}      [description]
- */
-function appendToEl(el, str) {
-	el.innerHTML = str
-}
-
-/**
- * [getSequence Dummy Code to return the 4onTheFloor sequence]
- * @return {object}
- */
-function getSequence() {
-	return {
-		name: "JS-808",
-		sampleMap: [
-			'Kick',
-			'Snare',
-			'Open Hat',
-			'Closed Hat'
-		],
-		pattern: {
-			seq: {
-				bpm: 128,
-				patternTracks: [
-					{
-						steps:[1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0]
-					},
-					{
-						steps:[0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0]
-					},
-					{
-						steps:[0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0]
-					},
-					{
-						steps:[1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0]
-					}
-				]
-			}
-		}
-	}
-}
-
-/**
- * [getControls description]
- * @return {arr} [description]
- */
-function getControls() {
-	return [
-		{
-			name: 'Play'
-		},
-		{
-			name: 'Stop'
-		}
-	]
 }
 
 /**
@@ -237,22 +64,97 @@ function template(str, obj) {
 }
 
 /**
- * [toggleClass from http://youmightnotneedjquery.com/]
- * @param  {DOM} el []
- * @return {sideeffect}    []
+ * [fetchSequence description]
+ * @param  {[type]} id [description]
+ * @return {[type]}    [description]
  */
-function toggleClass(el, className) {
-	if (el.classList) {
-		el.classList.toggle(className);
-	} else {
-		var classes = el.className.split(' ');
-		var existingIndex = classes.indexOf(className);
-
-		if (existingIndex >= 0)
-		  classes.splice(existingIndex, 1);
-		else
-		  classes.push(className);
-
-		el.className = classes.join(' ');
+function fetchSequence( id ) {
+	const sequences = {
+		'1': {
+			name: "JS-808",
+			sampleMap: [
+				'Kick',
+				'Snare',
+				'Open Hat',
+				'Closed Hat'
+			],
+			pattern: {
+				seq: {
+					bpm: 128,
+					patternTracks: [
+						{
+							steps:[1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0]
+						},
+						{
+							steps:[0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0]
+						},
+						{
+							steps:[0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0]
+						},
+						{
+							steps:[1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0]
+						}
+					]
+				}
+			}
+		},
+		'2': {},
+		'3': {},
 	}
+	return sequences[id] ? sequences[id] : sequences['1']
 }
+
+/**
+ * [renderBeatNumbers description]
+ * @return {[type]} [description]
+ */
+function renderBeatNumbers(){
+	let str = "";
+
+	if( ! state.data ) {
+		return str
+	}
+
+	let beatTemplate = `
+	<div class="col-15">
+	  <div class="beat-number">{idx}</div>
+	</div>
+	`
+
+	for (let i = 0; i < state.iterations; i++) {
+		str += template(beatTemplate, { idx : i + 1 })
+	}
+
+	return `<div class="flex space-evenly">${ str }</div>`
+}
+
+/**
+ * [renderTracks description]
+ * @return {[type]} [description]
+ */
+function renderTracks(){
+	return "Hi, I'm tracks thooooo"
+}
+
+/**
+ * [render description]
+ * @return {[type]} [description]
+ */
+function render() {
+	const appStr = [
+		renderBeatNumbers(),
+		renderTracks()
+	].join('')
+	app.innerHTML = appStr
+}
+
+/**
+ * [description]
+ * @param  {[type]} "DOMContentLoaded" [description]
+ * @param  {[type]} (                  [description]
+ * @return {[type]}                    [description]
+ */
+document.addEventListener("DOMContentLoaded", () => {
+	state.data = fetchSequence()
+	render();
+})
