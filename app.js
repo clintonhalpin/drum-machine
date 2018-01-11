34dad4a679c6f7dc3b2b0f0d679129ba82c53b3a
clintonhalpin <clintonhalpin@gmail.com>
2018-01-11 07:14:38 -0700

    Clean up

--- app.js
+++ app.js
@@ -1,77 +1,74 @@
 /**
  * UI Components
  */
-const app = document.getElementById('app')
-const playPause = document.getElementById('playPause')
-const speed = document.getElementById('speed')
-const trackSelect = document.getElementById('trackSelect')
+const app = document.getElementById("app");
+const playPause = document.getElementById("playPause");
+const speed = document.getElementById("speed");
+const trackSelect = document.getElementById("trackSelect");
 
 /**
  * State
  */
 let state = {
-	currIteration: 0,
-	data: false,
-	interval: false,
-	iterations: 16,
-	lastNodes: [],
-	playing: false,
-	timeout: 128,
-}
+  currIteration: 0,
+  data: false,
+  interval: false,
+  iterations: 16,
+  lastNodes: [],
+  playing: false,
+  timeout: 128
+};
 
 /**
  * Hook up our UI Event listeners
  */
-speed.addEventListener('change', modifyBPM)
-playPause.addEventListener('click', togglePlay) 
-trackSelect.addEventListener('change', changeTrack)
+speed.addEventListener("change", modifyBPM);
+playPause.addEventListener("click", togglePlay);
+trackSelect.addEventListener("change", changeTrack);
 
 /**
- * [description]
- * @param  {[type]} "DOMContentLoaded" [description]
- * @param  {[type]} (                  [description]
- * @return {[type]}                    [description]
+ * Fetch the Initial data, and render the app
  */
 document.addEventListener("DOMContentLoaded", () => {
-	state.data = fetchSequence()
-	render()
-})
-
+  state.data = fetchSequence();
+  render();
+});
 
 /**
  * [runSequence description]
  * @return {[type]} [description]
  */
-function runSequence(){
-	/**
-	 * Create Interval Timer
-	 */
-	state.interval = setInterval(() => {
-		/**
-		 * Loop through the last nodes/beats and reset classnames
-		 */
-		if( state.lastNodes.length > 0 ) {
-			state.lastNodes.map(node => {
-				toggleClass(node, 'beat-on')
-			})
-			state.lastNodes = []
-		}
-		/**
-		 * Update currentIteration ( bar )
-		 */
-		state = Object.assign(state, {
-			currIteration: state.currIteration < (state.iterations - 1) ? state.currIteration + 1 : 0
-		})
-		/**
-		 * Fetch the beats with class names that matech currentIteration
-		 * push them into lastNotes so we can clean up next time around!
-		 */
-		let nodes = document.querySelectorAll('.beat-' + state.currIteration);
-		for(var i = 0; i < nodes.length; i++) {
-			state.lastNodes.push(nodes[i])
-			toggleClass(nodes[i], 'beat-on')
-		}
-	}, state.timeout)
+function runSequence() {
+  /**
+   * Create Interval Timer
+   */
+  state.interval = setInterval(() => {
+    /**
+     * Loop through the last nodes/beats and reset classnames
+     */
+    if (state.lastNodes.length > 0) {
+      state.lastNodes.map(node => {
+        toggleClass(node, "beat-on");
+      });
+      state.lastNodes = [];
+    }
+    /**
+     * Update currentIteration ( bar )
+     */
+    state = Object.assign(state, {
+      currIteration:
+        state.currIteration < state.iterations - 1 ? state.currIteration + 1 : 0
+    });
+    /**
+     * Fetch the beats with class names that matech currentIteration
+     * push them into lastNotes so we can clean up next time around!
+     */
+    let nodes = document.querySelectorAll(".beat-" + state.currIteration);
+    for (var i = 0; i < nodes.length; i++) {
+      state.lastNodes.push(nodes[i]);
+      toggleClass(nodes[i], "beat-on");
+    }
+  }, state.timeout);
 }
 
 /**
@@ -80,54 +77,56 @@ function runSequence(){
  * @return {[type]}         [description]
  */
 function changePlayButtonText() {
-	toggleClass(playPause, 'red')
-	playPause.innerHTML = state.playBtnText
+  toggleClass(playPause, "red");
+  playPause.innerHTML = state.playBtnText;
 }
 
 /**
  * [modifyBPM description]
  * @return {[type]} [description]
  */
-function modifyBPM(e){
-	state = Object.assign(state, {
-		timeout: Math.floor(60000 / e.target.value) // @note this math isn't write
-	})
-	togglePlay()
-	togglePlay()
+function modifyBPM(e) {
+  state = Object.assign(state, {
+    timeout: Math.floor(60000 / e.target.value) // @note this math isn't write
+  });
+  togglePlay();
+  togglePlay();
 }
 
 /**
  * [togglePlay description]
  * @return {[type]} [description]
  */
-function togglePlay(){
+function togglePlay() {
+  if (state.playing) {
+    clearInterval(state.interval);
+    state = Object.assign(
+      state,
+      {
+        playBtnText: "Play",
+        playing: !state.playing
+      },
+      state
+    );
+    changePlayButtonText();
+    return true;
+  }
 
-	if( state.playing ) {
-		clearInterval(state.interval)
-		state = Object.assign(state, {
-			playBtnText: 'Play',
-			playing: ! state.playing
-		}, state)
-		changePlayButtonText()
-		return true
-	}
+  state = Object.assign(state, {
+    playBtnText: "Stop",
+    playing: !state.playing
+  });
+  changePlayButtonText();
 
-	state = Object.assign(state, {
-		playBtnText: 'Stop',
-		playing: ! state.playing
-	})
-	changePlayButtonText()
-
-	runSequence()
-
+  runSequence();
 }
 
-function changeTrack(e){
-	let trackId = e.target.value.replace('JS-', '')
-	state = Object.assign(state, {
-		data: fetchSequence(trackId)
-	})
-	render()
+function changeTrack(e) {
+  let trackId = e.target.value.replace("JS-", "");
+  state = Object.assign(state, {
+    data: fetchSequence(trackId)
+  });
+  render();
 }
 
 /**
@@ -137,12 +136,12 @@ function changeTrack(e){
  * @return {string}     [the template and data full rendered]
  */
 function template(str, obj) {
-	const keys = Object.keys(obj)
-	keys.map(item => {
-		let regex = new RegExp(`{${item}}`, 'g');
-		str = str.replace(regex, obj[item]);
-	})
-	return str
+  const keys = Object.keys(obj);
+  keys.map(item => {
+    let regex = new RegExp(`{${item}}`, "g");
+    str = str.replace(regex, obj[item]);
+  });
+  return str;
 }
 
 /**
@@ -151,19 +150,17 @@ function template(str, obj) {
  * @return {sideeffect}    []
  */
 function toggleClass(el, className) {
-	if (el.classList) {
-		el.classList.toggle(className);
-	} else {
-		var classes = el.className.split(' ');
-		var existingIndex = classes.indexOf(className);
+  if (el.classList) {
+    el.classList.toggle(className);
+  } else {
+    var classes = el.className.split(" ");
+    var existingIndex = classes.indexOf(className);
 
-		if (existingIndex >= 0)
-		  classes.splice(existingIndex, 1);
-		else
-		  classes.push(className);
+    if (existingIndex >= 0) classes.splice(existingIndex, 1);
+    else classes.push(className);
 
-		el.className = classes.join(' ');
-	}
+    el.className = classes.join(" ");
+  }
 }
 
 /**
@@ -171,118 +168,103 @@ function toggleClass(el, className) {
  * @param  {[type]} id [description]
  * @return {[type]}    [description]
  */
-function fetchSequence( id ) {
-	const sequences = {
-		'1': {
-			name: "JS-808",
-			sampleMap: [
-				'Kick',
-				'Snare',
-				'Open Hat',
-				'Closed Hat'
-			],
-			pattern: {
-				seq: {
-					bpm: 128,
-					patternTracks: [
-						{
-							steps:[1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0]
-						},
-						{
-							steps:[0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0]
-						},
-						{
-							steps:[0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0]
-						},
-						{
-							steps:[1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0]
-						}
-					]
-				}
-			}
-		},
-		'2': {
-			name: "JS-2",
-			sampleMap: [
-				'Kick',
-				'Snare',
-			],
-			pattern: {
-				seq: {
-					bpm: 128,
-					patternTracks: [
-						{
-							steps:[1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0]
-						},
-						{
-							steps:[0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1]
-						},
-					]
-				}
-			}
-		},
-		'3': {
-			name: "JS-2",
-			sampleMap: [
-				'Kick',
-				'Snare',
-				'Snare',
-				'Snare',
-				'Snare',
-				'Snare',
-			],
-			pattern: {
-				seq: {
-					bpm: 128,
-					patternTracks: [
-						{
-							steps:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-						},
-						{
-							steps:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-						},
-						{
-							steps:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-						},
-						{
-							steps:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-						},
-						{
-							steps:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-						},
-						{
-							steps:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-						},
-					]
-				}
-			}
-		},
-	}
-	return sequences[id] ? sequences[id] : sequences['1']
+function fetchSequence(id) {
+  const sequences = {
+    "1": {
+      name: "JS-808",
+      sampleMap: ["Kick", "Snare", "Open Hat", "Closed Hat"],
+      pattern: {
+        seq: {
+          bpm: 128,
+          patternTracks: [
+            {
+              steps: [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0]
+            },
+            {
+              steps: [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+            },
+            {
+              steps: [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0]
+            },
+            {
+              steps: [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0]
+            }
+          ]
+        }
+      }
+    },
+    "2": {
+      name: "JS-2",
+      sampleMap: ["Kick", "Snare"],
+      pattern: {
+        seq: {
+          bpm: 128,
+          patternTracks: [
+            {
+              steps: [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0]
+            },
+            {
+              steps: [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1]
+            }
+          ]
+        }
+      }
+    },
+    "3": {
+      name: "JS-2",
+      sampleMap: ["Kick", "Snare", "Snare", "Snare", "Snare", "Snare"],
+      pattern: {
+        seq: {
+          bpm: 128,
+          patternTracks: [
+            {
+              steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            },
+            {
+              steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            },
+            {
+              steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            },
+            {
+              steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            },
+            {
+              steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            },
+            {
+              steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            }
+          ]
+        }
+      }
+    }
+  };
+  return sequences[id] ? sequences[id] : sequences["1"];
 }
 
 /**
  * [renderBeatNumbers description]
  * @return {[type]} [description]
  */
-function renderBeatNumbers(){
-	let str = ""
+function renderBeatNumbers() {
+  let str = "";
 
-	if( ! state.data ) {
-		return str
-	}
+  if (!state.data) {
+    return str;
+  }
 
-	let beatTemplate = `
+  let beatTemplate = `
 	<div class="col-15">
 	  <div class="beat-number">{idx}</div>
 	</div>
-	`
+	`;
 
-	for (let i = 0; i < state.iterations; i++) {
-		str += template(beatTemplate, { idx : i + 1 })
-	}
+  for (let i = 0; i < state.iterations; i++) {
+    str += template(beatTemplate, { idx: i + 1 });
+  }
 
-	return `<div class="flex space-evenly">${ str }</div>`
+  return `<div class="flex space-evenly">${str}</div>`;
 }
 
 /**
@@ -291,34 +273,41 @@ function renderBeatNumbers(){
  * @return {[type]}      [description]
  */
 function renderTrackBeats(idx, track) {
-	let beatTemplate = `
+  let beatTemplate = `
 	<div class="col-15">
 		<div class="beat {customClass}">
 		    <div class="beat-inner track-{trackIdx} beat-{beatIdx}"></div>
 		</div>
 	</div>
-	`
-	return track.steps.map((beat, jdx) => {
-		  let tD = {
- 				trackIdx: idx, 
- 				beatIdx: jdx, 
- 				customClass: beat ? 'beat--active' : '' 
-		  }
-			return template(beatTemplate, tD)
-	}).join('')
+	`;
+  return track.steps
+    .map((beat, jdx) => {
+      let tD = {
+        trackIdx: idx,
+        beatIdx: jdx,
+        customClass: beat ? "beat--active" : ""
+      };
+      return template(beatTemplate, tD);
+    })
+    .join("");
 }
 
 /**
  * [renderTracks description]
  * @return {[type]} [description]
  */
-function renderTracks(){
-	if( ! state.data ) {
-		return ""
-	}
-	return state.data.pattern.seq.patternTracks.map((track, idx) => {
-		return `<div class="track flex space-evenly white">${ renderTrackBeats(idx, track) }</div>`
-	}).join('')
+function renderTracks() {
+  if (!state.data) {
+    return "";
+  }
+  return state.data.pattern.seq.patternTracks
+    .map((track, idx) => {
+      return `<div class="track flex space-evenly white">${renderTrackBeats(
+        idx,
+        track
+      )}</div>`;
+    })
+    .join("");
 }
 
 /**
@@ -326,8 +315,5 @@ function renderTracks(){
  * @return {[type]} [description]
  */
 function render() {
-	app.innerHTML = [
-		renderBeatNumbers(),
-		renderTracks()
-	].join('')
+  app.innerHTML = [renderBeatNumbers(), renderTracks()].join("");
 }
